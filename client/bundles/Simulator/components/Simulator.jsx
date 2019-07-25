6d159204a4589656a8b7b58e608e1dbbd4a4537e
Flavio <flavio@algorich.com.br>
2019-07-25 14:59:06 -0300

    finish simulator mobile layout

--- client/bundles/Simulator/components/Simulator.jsx
+++ client/bundles/Simulator/components/Simulator.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import $ from 'jquery';
+import dolar from 'images/ic_dolar_24px.png'
+import pinDrop from 'images/ic_pin_drop_24px.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default class Simulator extends React.Component {
   constructor(props) {
@@ -11,7 +15,7 @@ export default class Simulator extends React.Component {
     };
   }
 
-  handleFormSelect() {
+  handleOptionSelected() {
     const code = this.state.code;
     const $options = $('.form__select option');
 
@@ -24,8 +28,15 @@ export default class Simulator extends React.Component {
     });
   }
 
+  handleSelect() {
+    $('.form__select--coin').on('change', function() {
+      $('.form').submit();
+    });
+  }
+
   componentDidMount() {
-    this.handleFormSelect();
+    this.handleOptionSelected();
+    this.handleSelect();
   }
 
   render() {
@@ -37,21 +48,40 @@ export default class Simulator extends React.Component {
         </div>
 
         <div className='simulator__body'>
-          <div className='title'>
+          <div className='simulator__title'>
             {this.state.fullName}
-            <span className='title--value'>{this.state.value}</span>
+            <span className='simulator__title--value'>R$ {this.state.value}</span>
           </div>
 
           <form action='/simulate' method='post' className='form'>
-            <select className='form__select' name='code'>
-              <option value='USD'>USD</option>
-              <option value='EUR'>EUR</option>
-            </select>
+            <div className='form__select-box'>
+              <div className='select-box__icon'>
+                <img src={pinDrop} id='pin-drop'/>
+              </div>
+              <select className='form__select form__select--state' name='state'>
+                <option value='RJ'>Rio de Janeiro</option>
+              </select>
+              <FontAwesomeIcon icon={faSearch}/>
+            </div>
 
-            <input className='form__buttom' type='submit'/>
+            <div className='form__select-box'>
+              <div className='select-box__icon'>
+                <img src={dolar} id='dolar'/>
+              </div>
+              <select className='form__select form__select--coin' name='code'>
+                <option value='USD'>USD</option>
+                <option value='EUR'>EUR</option>
+              </select>
+            </div>
+
+            <button className='form__buttom' type='button'>
+              Comparar casas de câmbio
+            </button>
           </form>
         </div>
       </div>
     );
   }
 }
+
+// <input className='form__buttom' type='submit'/>
