d585f09134f745b6d25ac56bf3b7c6c1ba94c765
Flavio <flavio@algorich.com.br>
2019-07-25 16:29:42 -0300

    add states select to form

--- client/bundles/Simulator/components/Simulator.jsx
+++ client/bundles/Simulator/components/Simulator.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import $ from 'jquery';
-import dolar from 'images/ic_dolar_24px.png'
-import pinDrop from 'images/ic_pin_drop_24px.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import Form from './Form';
 
 export default class Simulator extends React.Component {
   constructor(props) {
@@ -13,30 +9,6 @@ export default class Simulator extends React.Component {
       fullName: this.props.full_name,
       code: this.props.code
     };
-  }
-
-  handleOptionSelected() {
-    const code = this.state.code;
-    const $options = $('.form__select option');
-
-    $('.form__select option[selected]').removeAttr('selected');
-
-    $options.each(function() {
-      if ($(this).val() == code) {
-        $(this).attr('selected', 'selected');
-      }
-    });
-  }
-
-  handleSelect() {
-    $('.form__select--coin').on('change', function() {
-      $('.form').submit();
-    });
-  }
-
-  componentDidMount() {
-    this.handleOptionSelected();
-    this.handleSelect();
   }
 
   render() {
@@ -53,31 +25,7 @@ export default class Simulator extends React.Component {
             <span className='simulator__title--value'>R$ {this.state.value}</span>
           </div>
 
-          <form action='/simulate' method='post' className='form'>
-            <div className='form__select-box'>
-              <div className='select-box__icon'>
-                <img src={pinDrop} id='pin-drop'/>
-              </div>
-              <select className='form__select form__select--state' name='state'>
-                <option value='RJ'>Rio de Janeiro</option>
-              </select>
-              <FontAwesomeIcon icon={faSearch}/>
-            </div>
-
-            <div className='form__select-box'>
-              <div className='select-box__icon'>
-                <img src={dolar} id='dolar'/>
-              </div>
-              <select className='form__select form__select--coin' name='code'>
-                <option value='USD'>USD</option>
-                <option value='EUR'>EUR</option>
-              </select>
-            </div>
-
-            <button className='form__buttom' type='button'>
-              Comparar casas de câmbio
-            </button>
-          </form>
+          <Form code={this.state.code}/>
         </div>
       </div>
     );
